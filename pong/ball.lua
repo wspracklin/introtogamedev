b85954776dbbb9666f63e8effd06ae205af3f8e9
Bill Spracklin <williamspracklin@gmail.com>
2022-03-16 18:06:40 -0400

    Day4

--- pong/ball.lua
+++ pong/ball.lua
@@ -3,12 +3,14 @@ require "libs.classic"
 Ball = Object:extend()
 
 function Ball:new()
-    self.speed = 0
-    self.radius = 0
-    self.xPosition = 0 
-    self.yPosition = 0
-    self.xDirection = 0 * self.speed
+    self.speed = 200
+    self.radius = 10
+    self.xPosition = love.graphics.getWidth() / 2 
+    self.yPosition = love.graphics.getHeight() / 2 
+    self.xDirection = 1 * self.speed
     self.yDirection = 0 * self.speed
+    self.screenWidth = love.graphics.getWidth()
+    self.screenHeight = love.graphics.getHeight()
 end
 
 function Ball:checkCollision()
@@ -16,13 +18,22 @@ function Ball:checkCollision()
 
     -- "rightWall", "leftWall", "topWall", "bottomWall"
 
-    screenWidth = love.graphics.getWidth()
-    screenHeight = love.graphics.getHeight()
+    if self.xPosition >= self.screenWidth then return "rightWall"
+    elseif self.xPosition < 0 then return "leftWall"
+    end
 
    return ""
 end
 
 function Ball:move(millisecondsBetweenFrame)
+    collision = self:checkCollision()
+
+    if( collision == "rightWall" or collision == "leftWall" )
+    then self.xDirection = self.xDirection * -1
+    end
+
+    self.xPosition = self.xPosition + self.xDirection * millisecondsBetweenFrame
+
 end
 
 function Ball:draw()
